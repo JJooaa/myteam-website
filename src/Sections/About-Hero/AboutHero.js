8c1f2f32db313b1381d5afbdf4663c50bb678711
JJooaa <leitola.joa@gmail.com>
2021-12-15 15:57:50 +0200

    finish about page hero section for 1440px

--- src/Sections/About-Hero/AboutHero.js
+++ src/Sections/About-Hero/AboutHero.js
@@ -2,15 +2,27 @@ import React from "react";
 import "./AboutHero.css";
 import NavBar from "../../Components/NavBar/NavBar";
 
+// About page hero section
+
 const AboutHeroSection = () => {
     return (
         <section id="about-hero">
             <NavBar />
-            <div className="about-hero-info">
-                
+            <div className="about-hero-wrapper">
+                <h1 style={{ fontSize: 64 }}>About</h1>
+                <div className="about-hero-info">
+                    <p>
+                        We help companies build dynamic teams made up of top
+                        global talent. Using our network of passionate
+                        professionals we drive innovation and deliver incredible
+                        outcomes. Talented, diverse teams shape the best
+                        products and experiences. We’ll bring those teams to
+                        you.
+                    </p>
+                </div>
             </div>
         </section>
-    )
+    );
 };
 
-export default AboutHeroSection
+export default AboutHeroSection;
