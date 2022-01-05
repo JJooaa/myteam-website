ded56d3f499c4f5317e89d2694c6613ab24f89f2
JJooaa <leitola.joa@gmail.com>
2022-01-05 09:37:47 +0200

    allow contactbutton to change page via context

--- src/Components/ContactButton/ContactButton.js
+++ src/Components/ContactButton/ContactButton.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./ContactButton.css";
+import { PageContext } from "../../App";
 
 const ContactButton = ({ color }) => {
+    const [page, setPage] = useContext(PageContext);
+
     return (
-        <div style={{ borderColor: color }} className="contact-button">
+        <div
+            style={{ borderColor: color }}
+            className="contact-button"
+            onClick={() => setPage("contact")}
+        >
             <p style={{ color: color }}>contact us</p>
         </div>
     );
